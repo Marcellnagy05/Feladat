7505dd1872cc3e3b426c34f450678013c5320751
Marcellnagy05 <70641505+Marcellnagy05@users.noreply.github.com>
2023-11-14 12:22:32 +0100

    Add files via upload

---
+++ ConsoleApp3/Balkezesek.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Balkezesek
+    {
+        public string name { get; }
+        public DateTime first { get; }
+        public DateTime last { get; }
+        public double weight { get; }
+        public double height { get; }
+    }
+}
---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            string[] lines = File.ReadAllLines("balkezesek.csv");
+
+            List<Balkezesek> BalkezesekList = new List<Balkezesek>();
+
+            for (int i = 1; i < lines.Length; i++)
+            {
+                Balkezesek Balkezesek = new Balkezesek(lines[i]);
+
+                BalkezesekList.Add(Balkezesek);
+            }
+
+            Console.WriteLine("3. feladat:");
+            Lines(BalkezesekList);
+
+            Console.WriteLine("4. feladat:");
+            Datas(BalkezesekList);
+
+    
+        }
+        private static void Lines(List<Balkezesek> BalkezesekList)
+        {
+            Console.WriteLine(BalkezesekList.Count);
+        }
+        private static void Datas(List<Balkezesek> BalkezesekList)
+        {
+            DateTime dateBeginning = new DateTime(1999, 10, 01);
+            DateTime dateEnd = new DateTime(1999, 10, 31);
+            foreach (var baseballPlayer in BalkezesekList)
+            {
+                if (Balkezesek.last > dateBeginning && Balkezesek.last < dateEnd)
+                {
+                    Console.WriteLine(Balkezesek.name + ", " + String.Format("{0:0.0}", Balkezesek.height * 2.54) + " cm");
+                }
+            }
+        }
+
+    }
+}
